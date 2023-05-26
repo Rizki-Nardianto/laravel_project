37b498c6beeb006fe7da6cde11e18055e72f5d10
Rizki Nardianto <rizkinardianto03@gmail.com>
2023-05-26 10:48:35 +0700

    upload file laravel_project

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rizki Nardianto",
+        "email" => "rizkinardianto03@gmail.com",
+        "image" => "rizki.jpg"
+    ]);
+});
+Route::get('/post', function () {
+    return view('post');
+});
